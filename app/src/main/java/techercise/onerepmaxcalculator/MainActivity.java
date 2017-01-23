b7e0e1255fa0d3195b9593dd5beadcd3a65477a7
techercise <mjl1997@verizon.net>
2017-01-23 14:54:00 -0500

    2nd Commit
    In this commit, the Epley ORM Formula is displayed via a long display of the Toast. To show this, a submit button was added. On the click of the button, the toast displays the result of the Epley formula which incorporates editText and editText2.

--- app/src/main/java/techercise/onerepmaxcalculator/MainActivity.java
+++ app/src/main/java/techercise/onerepmaxcalculator/MainActivity.java
@@ -2,12 +2,24 @@ package techercise.onerepmaxcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public EditText numReps;
+    public EditText amtWeight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        numReps= (EditText) findViewById(R.id.editText2);
+        amtWeight= (EditText) findViewById(R.id.editText);
+    }
+
+    public void buttonSubmit(View view)
+    {
+        Toast.makeText(view.getContext(),"Epley Formula: " + (Integer.parseInt(amtWeight.getText().toString()) * (1 + Integer.parseInt(numReps.getText().toString())/30)) + " lbs.", Toast.LENGTH_LONG).show();
     }
 }
