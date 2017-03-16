38e35d29dc951a03fffdbb615d86c42adf5698ea
techercise <mjl1997@verizon.net>
2017-03-15 20:02:53 -0400

    5th Commit
    In this commit, the format was changed to show the results of all seven ORM formulas versus displaying all seven and the average separately. More formatting changes were also made. Should be final commit before release to Google Play Store, with a possible overhaul coming in the next update. Initial goal was to release an app before I turn 20.

--- app/src/main/java/techercise/onerepmaxcalculator/MainActivity.java
+++ app/src/main/java/techercise/onerepmaxcalculator/MainActivity.java
@@ -13,33 +13,52 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    public TextView epRes;
+    public TextView results;
     public EditText numReps;
     public EditText amtWeight;
-    public TextView resText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         numReps= (EditText) findViewById(R.id.editText2);
         amtWeight= (EditText) findViewById(R.id.editText);
-        resText = (TextView) findViewById(R.id.textView5);
-        resText.setTypeface(null, Typeface.BOLD);
+
     }
 
     public void buttonSubmit(View view)
     {
-        epRes = (TextView) findViewById(R.id.textView4);
-        epRes.setText((Integer.parseInt(amtWeight.getText().toString()) * (1 + Integer.parseInt(numReps.getText().toString()) / 30)) + " lbs.");
-        epRes.setTypeface(null, Typeface.BOLD);
+        int weight = (Integer.parseInt(amtWeight.getText().toString()));
+        int reps = Integer.parseInt(numReps.getText().toString());
+        //results of Epley formula
+        double epRes = (weight * (1 + reps/30));
+        //results of Brzycki formula
+        double brzyRes = weight * (36/(37 - reps));
+        //results of McGlothlin formula
+        double mcRes = (100 * weight)/(101.3 - (2.67123 - reps));
+        //results of Lombardi formula
+        double lomRes = weight * Math.pow(reps,0.1);
+        //results of Mayhew formula
+        double mayRes = (100 * weight)/(52.2 + (41.9 * Math.pow(Math.E, (-0.055 * reps))));
+        //results of O'Conner formula
+        double oconRes = weight * (1 + (reps/40));
+        //reps of Wathan formula
+        double wathRes = (100 * weight)/(48.8 + (Math.pow((53.8*Math.E), (-0.075*reps))));
+        //find average of all formulas
+        int avgRes = (int) Math.round((epRes + brzyRes + mcRes + lomRes + mayRes + oconRes +
+                                        wathRes)/7);
+        //display results in results bar
+        results = (TextView) findViewById(R.id.textView4);
+        results.setText(avgRes + " lbs.");
+        //bold results to make them easier to see for user
+        results.setTypeface(null, Typeface.BOLD);
     }
 
     public void clearSubmit(View view)
     {
-        epRes = (TextView) findViewById(R.id.textView4);
+        results = (TextView) findViewById(R.id.textView4);
         numReps = (EditText) findViewById(R.id.editText2);
         amtWeight = (EditText) findViewById(R.id.editText);
-        epRes.setText("");
+        results.setText("");
         numReps.setText("");
         amtWeight.setText("");
     }
