f386ecf7e75ac5ea17d4c814c93fb219ef2ac3e9
techercise <mjl1997@verizon.net>
2017-03-21 15:46:33 -0400

    7th Commit
    Fixed a crash that occured when the user hit the submit button and nothing was typed in editText and/or editText2. Now, an appropiate message is displayed.

--- app/src/main/java/techercise/onerepmaxcalculator/MainActivity.java
+++ app/src/main/java/techercise/onerepmaxcalculator/MainActivity.java
@@ -5,11 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,39 +18,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         numReps= (EditText) findViewById(R.id.editText2);
         amtWeight= (EditText) findViewById(R.id.editText);
-
+        //display results in designated textView
+        results = (TextView) findViewById(R.id.textView4);
     }
 
     public void buttonSubmit(View view)
     {
-        //display results in designated textView
-        results = (TextView) findViewById(R.id.textView4);
-        int weight = (Integer.parseInt(amtWeight.getText().toString()));
-        int reps = Integer.parseInt(numReps.getText().toString());
-        //results of Epley formula
-        double epRes = (weight * (1 + reps/30));
-        //results of Brzycki formula
-        double brzyRes = weight * (36/(37 - reps));
-        //results of McGlothlin formula
-        double mcRes = (100 * weight)/(101.3 - (2.67123 - reps));
-        //results of Lombardi formula
-        double lomRes = weight * Math.pow(reps,0.1);
-        //results of Mayhew formula
-        double mayRes = (100 * weight)/(52.2 + (41.9 * Math.pow(Math.E, (-0.055 * reps))));
-        //results of O'Conner formula
-        double oconRes = weight * (1 + (reps/40));
-        //reps of Wathan formula
-        double wathRes = (100 * weight)/(48.8 + (Math.pow((53.8*Math.E), (-0.075*reps))));
-        //find average of all formulas
-        int avgRes = (int) Math.round((epRes + brzyRes + mcRes + lomRes + mayRes + oconRes +
-                                        wathRes)/7);
-        if(amtWeight.getText().toString() == null || numReps.getText().toString() == null ||
-                (amtWeight.getText().toString() == null && numReps.getText().toString() == null))
+        if(amtWeight.getText().toString().equals("") || numReps.getText().toString().equals("") ||
+                (amtWeight.getText().toString().equals("") && numReps.getText().toString().equals("")))
         {
             results.setText("Enter reps and weight!");
         }
-        else
-        {
+        else {
+            int weight = (Integer.parseInt(amtWeight.getText().toString()));
+            int reps = Integer.parseInt(numReps.getText().toString());
+            //results of Epley formula
+            double epRes = (weight * (1 + reps / 30));
+            //results of Brzycki formula
+            double brzyRes = weight * (36 / (37 - reps));
+            //results of McGlothlin formula
+            double mcRes = (100 * weight) / (101.3 - (2.67123 - reps));
+            //results of Lombardi formula
+            double lomRes = weight * Math.pow(reps, 0.1);
+            //results of Mayhew formula
+            double mayRes = (100 * weight) / (52.2 + (41.9 * Math.pow(Math.E, (-0.055 * reps))));
+            //results of O'Conner formula
+            double oconRes = weight * (1 + (reps / 40));
+            //reps of Wathan formula
+            double wathRes = (100 * weight) / (48.8 + (Math.pow((53.8 * Math.E), (-0.075 * reps))));
+            //find average of all formulas
+            int avgRes = (int) Math.round((epRes + brzyRes + mcRes + lomRes + mayRes + oconRes +
+                    wathRes) / 7);
             results.setText(avgRes + " lbs.");
             //bold results to make them easier to see for user
             results.setTypeface(null, Typeface.BOLD);
